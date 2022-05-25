bbd71e74f3baabd34b95d5f16831368782c35334
Abhisek Joshi <nofursad@gmail.com>
2022-05-25 08:33:09 +0000

    Add Products app, models and fixtures

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-
+    'products',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',

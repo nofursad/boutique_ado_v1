96c18f8fcbfcb9652475b32957e24de5ff869c45
Abhisek Joshi <nofursad@gmail.com>
2022-05-30 14:05:00 +0000

    updated shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -17,7 +17,7 @@ def bag_contents(request):
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
-            'products': product,
+            'product': product,
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
@@ -26,9 +26,9 @@ def bag_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,

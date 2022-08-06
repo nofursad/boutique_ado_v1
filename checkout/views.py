4b7bd21d39af6476d2421b63838a15ed5679d977
Abhisek Joshi <nofursad@gmail.com>
2022-08-06 08:12:34 +0000

    Added functionality to accept the payment and process through stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in your bag at the moment")
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in your cart at the moment")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
+        'stripe_public_key': 'pk_test_51L8TgLAwRE7erEoo18UbhTeUFJowDTamLgVCUocP4CYTyyWFXaEfXIJrqWtrSGuvDLyyKZjEiNFFBghDJfVSdNYv00ie5D0uEA',
         'client_secret': 'test client secret',
     }
 
